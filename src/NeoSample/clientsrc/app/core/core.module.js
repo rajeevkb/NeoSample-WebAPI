e1dc9ca95819ad6a243ef35471a0318da0d4df18
Swagger of the loaf kind <swaggerloaf@gmail.com>
2015-03-28 14:17:49 -0400

    Update core.module.js

--- src/NeoSample/clientsrc/app/core/core.module.js
+++ src/NeoSample/clientsrc/app/core/core.module.js
@@ -6,10 +6,6 @@
             /* Angular modules */
             'ngAnimate',
             'ngSanitize',
-            /* Cross-app modules */
-            //'blocks.exception',
-            //'blocks.logger',
-            //'blocks.router',
             /* 3rd-party modules */
             'ui.router'
         ]);
